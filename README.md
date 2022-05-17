1460c1346fed2f8b80aad21ed9f9102893ef5426
Adham Salama <adhamsalama@zohomail.com>
2022-05-17 20:17:36 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# PDFSEARCH
+
+This is a CLI application to search pdf
+
+# How to setup
+
+```
+sudo apt install libpoppler-cpp-dev
+pip install -r requirements.txt
+```
+
+### How to use:
+
+```
+Usage: pdfsearch.py [OPTIONS] FILENAME QUERY...
+
+Arguments:
+  FILENAME  [required]
+  QUERY...  [required]
+
+Options:
+  --ignorecase / --no-ignorecase  Ignore case for searching  [default:
+                                  ignorecase]
+  --exact / --no-exact            Match exact substring  [default: no-exact]
+  --install-completion [bash|zsh|fish|powershell|pwsh]
+                                  Install completion for the specified shell.
+  --show-completion [bash|zsh|fish|powershell|pwsh]
+                                  Show completion for the specified shell, to
+                                  copy it or customize the installation.
+  --help                          Show this message and exit.
+
+```
+
+### An example:
+
+```
+./pdfsearch.py test.pdf title sample ample lorem ore
+```
+
+![Output](images/output.png)
---
+++ pdfsearch.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python
+from typing import cast, Any
+import typer
+import pdftotext  # type: ignore
+import re
+app = typer.Typer()
+
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
+@app.command()
+def search(filename: str, query: list[str], ignorecase: bool = typer.Option(True, help='Ignore case for searching'), exact: bool = typer.Option(False, help='Match exact substring')):
+    with open(filename, 'rb') as file:
+        pdf = pdftotext.PDF(file)
+        results: list[Any] = []
+        regex_list: list[str] = []
+        for word in query:
+            regex_list.append(re.escape(word))
+        if exact:
+            regex_pattern = '(' + '|'.join(regex_list) + ')'
+        else:
+            regex_pattern = '.*(?:' + '|'.join(regex_list) + ').*'
+        if ignorecase:
+            regex = re.compile(regex_pattern, re.IGNORECASE)
+        else:
+            regex = re.compile(regex_pattern)
+        matches: list[str] = []
+        for page in pdf:
+            # find matches
+            for x in regex.findall(page):
+                # color matches
+                colored = x
+                matches_to_color = re.compile(
+                    '.*(' + '|'.join(regex_list) + ').*', re.IGNORECASE).findall(x)
+                for m in matches_to_color:
+                    colored = colored.replace(
+                        m, Color.GREEN + Color.BOLD + Color.UNDERLINE + m + Color.END)
+                matches.append(colored)
+        for result in matches:
+            print(result)
+
+
+if __name__ == '__main__':
+    app()
